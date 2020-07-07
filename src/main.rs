b535404b4d84510032c877892c3189f43726abe4
WAU4026 <joohae5@gmail.com>
2020-07-08 03:05:56 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("test");
+    println!("숫자를 입력하세요");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+    .expect("error1");
+
+    println!("입력한 숫자 : {}", guess);
 }
