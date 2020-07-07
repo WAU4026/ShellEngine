703451669e3f7cf0d3429b927813d2d62c9de8f9
WAU4026 <joohae5@gmail.com>
2020-07-08 03:54:17 +0900

    test2

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
 use std::io;
+use rand;
+use std::cmp::Ordering;
+
+use rand::Rng;
 
 fn main() {
     println!("test");
     println!("숫자를 입력하세요");
 
-    let mut guess = String::new();
+    let mut s : bool = true;
+    while(s){
+        let mut guess = String::new();
+        let rnd = rand::thread_rng().gen_range(1, 11);
 
-    io::stdin().read_line(&mut guess)
-    .expect("error1");
+        io::stdin().read_line(&mut guess)
+        .expect("error1");
 
-    println!("입력한 숫자 : {}", guess);
+        let guess : i32 = guess.trim().parse().expect("숫자를 입력하세요.");
+
+        println!("입력한 숫자 : {}, 랜덤한 숫자 : {}", guess, rnd);
+
+        match guess.cmp(&rnd){
+            Ordering::Less => println!("랜덤 수 보다 작습니다."),
+            Ordering::Equal => s = false,
+            Ordering::Greater => println!("랜덤 수 보다 큽니다."),
+
+        }
+    }
 }
